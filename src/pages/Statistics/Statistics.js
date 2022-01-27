21d6609e9b1a8942d2311c26e8975b5261cbd61f
Elumion <dovgalyukvlad87@gmail.com>
2022-01-27 23:50:19 +0200

    change page for displaying components, still in work

--- src/pages/Statistics/Statistics.js
+++ src/pages/Statistics/Statistics.js
@@ -1,5 +1,18 @@
-const Statistics = () => {
-    return <h2>Statistics view</h2>;
+import axios, { Axios } from "axios";
+import ReactDom from "react-dom";
+import React from "react";
+import { Statistics } from '../../components/Statistics';
+const StatisticsPage = (props) => {
+    return (
+        <>
+            <h2>
+                Statistics view
+            </h2>
+            <Statistics />
+
+        </>
+    )
 };
 
-export default Statistics;
+
+export default StatisticsPage;
