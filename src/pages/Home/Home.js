ccd2c06efd7f16bb3ad145078f3ebeca7a3d8423
Svyatoslav Huzar <huzarsv@outlook.com>
2022-01-27 23:58:31 +0200

    add transaction button implemented

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import {AddTransactionBtn} from './Home.styles';
 
 const Home = () => {
     return (
         <div>
             <h2>Home view</h2>
+
+            <AddTransactionBtn>
+                <AddIcon />
+            </AddTransactionBtn>
         </div>
     );
 };
