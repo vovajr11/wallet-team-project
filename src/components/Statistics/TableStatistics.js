5b3b8b5e1f1bd6e7cc4f98ad9eeb41dcb9edaee7
Elumion <dovgalyukvlad87@gmail.com>
2022-01-30 01:03:52 +0200

    refactor some code

--- src/components/Statistics/TableStatistics.js
+++ src/components/Statistics/TableStatistics.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { default as BasicSelect } from "./Select";
 import { SelectsContainer, TableContainer } from "./Statistics.styles";
 import { TableTransactions } from './TableTransactions';
+import { TransictionsList } from './TransictionsList';
 
 
 import axios from 'axios';
@@ -10,32 +11,6 @@ import { useSelector } from 'react-redux';
 
 const TableStatistics = () => {
 
-    const checker = useSelector(state => state.session);
-    console.log(checker);
-
-    axios.get("https://wallet.goit.ua/api/transactions-summary",
-        {
-            "month": 1,
-            "year": 2021,
-        })
-        .then(data => console.log(data.data));
-    axios.get("https://wallet.goit.ua/api/transactions",
-        {
-        })
-        .then(data => console.log(data.data));
-    axios.post("https://wallet.goit.ua/api/transactions",
-
-        {
-            "transactionDate": "2021-01-01",
-            "type": "INCOME",
-            "categoryId": "c9d9e447-1b83-4238-8712-edc77b18b739",
-            "comment": "Хліб",
-            "amount": 22
-        }
-    )
-        .then(data => console.log(data.data))
-        .catch(err => console.log(err));
-
     return (
         <TableContainer>
             <SelectsContainer>
@@ -43,6 +18,7 @@ const TableStatistics = () => {
                 <BasicSelect selectName="Year" />
             </SelectsContainer>
             <TableTransactions />
+            <TransictionsList />
         </TableContainer>
     )
 }
