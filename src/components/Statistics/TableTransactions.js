0a9af5329661c9d2060054553accde8932b2d172
Elumion <dovgalyukvlad87@gmail.com>
2022-01-29 23:40:22 +0200

    create component for transitions

---
+++ src/components/Statistics/TableTransactions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    DefinitionContainer
+} from './Statistics.styles'
+
+export const TableTransactions = () => {
+    return (
+        <>
+            <DefinitionContainer>
+                <p>Category</p>
+                <p>Amount</p>
+            </DefinitionContainer>
+        </>
+    )
+}
