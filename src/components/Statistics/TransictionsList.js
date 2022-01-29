fe815445d44bd581cc7e537ed350aea3c2cbfea3
Elumion <dovgalyukvlad87@gmail.com>
2022-01-30 01:05:02 +0200

    create new component that contain transition operations, still in work

---
+++ src/components/Statistics/TransictionsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ListOfTransitions, ListItem } from './Statistics.styles'
+
+export const TransictionsList = () => {
+    return (
+        <ListOfTransitions>
+            <ListItem>1</ListItem>
+            <ListItem>2</ListItem>
+            <ListItem>3</ListItem>
+        </ListOfTransitions>
+    )
+}
