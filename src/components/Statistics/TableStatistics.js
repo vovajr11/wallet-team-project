e53a63eac44460128343de13cfa9c16c93ba257c
Elumion <dovgalyukvlad87@gmail.com>
2022-01-29 23:11:21 +0200

    fetch request

--- src/components/Statistics/TableStatistics.js
+++ src/components/Statistics/TableStatistics.js
@@ -2,8 +2,38 @@ import React from 'react';
 import { default as BasicSelect } from "./Select";
 import { SelectsContainer } from "./Statistics.styles"
 
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+
 
 const TableStatistics = () => {
+
+    const checker = useSelector(state => state.session);
+    console.log(checker);
+
+    axios.get("https://wallet.goit.ua/api/transactions-summary",
+        {
+            "month": 1,
+            "year": 2021,
+        })
+        .then(data => console.log(data.data));
+    axios.get("https://wallet.goit.ua/api/transactions",
+        {
+        })
+        .then(data => console.log(data.data));
+    axios.post("https://wallet.goit.ua/api/transactions",
+
+        {
+            "transactionDate": "2021-01-01",
+            "type": "INCOME",
+            "categoryId": "c9d9e447-1b83-4238-8712-edc77b18b739",
+            "comment": "Хліб",
+            "amount": 22
+        }
+    )
+        .then(data => console.log(data.data))
+        .catch(err => console.log(err));
+
     return (
         <SelectsContainer>
             <BasicSelect selectName="Month" />
