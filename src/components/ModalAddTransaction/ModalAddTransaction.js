fd9ecd75b55cdd8bf2db56d61985e9677a3db80e
Svyatoslav Huzar <huzarsv@outlook.com>
2022-01-30 17:19:24 +0200

    Transactions category toggle button implementation

--- src/components/ModalAddTransaction/ModalAddTransaction.js
+++ src/components/ModalAddTransaction/ModalAddTransaction.js
@@ -1,22 +1,87 @@
 import React, { useState } from 'react';
-import { AddTransactionBtn} from './ModalAddTransaction.styles';
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import {ReactComponent as AddIcon} from '../../assets/svgs/plus.svg';
+import {
+    AddTransactionBtn,
+    Toggler,
+    ToggleP,
+    ToggleLabel,
+    ToggleInput,
+    ToggleBackground,
+    ToggleBtn,
+} from './ModalAddTransaction.styles';
+import { ReactComponent as AddIcon } from '../../assets/svgs/plus.svg';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+const defaultState = {
+    date: new Date(),
+    type: false,
+    amount: '',
+    comment: '',
+    category: '',
+};
 
 export default function ModalAddTransaction() {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    const [transaction, setTransaction] = useState(defaultState);
+
+    const handleInputChange = event => {
+        const name = event.target.name;
+        const value =
+            event.target.type === 'checkbox'
+                ? event.target.checked
+                : event.target.value;
+        updateTransaction(name, value);
+        if (event.target.type === 'checkbox') {
+            updateTransaction('category', '');
+        }
+    };
+
+    const updateTransaction = (name, value) => {
+        setTransaction(prev => ({ ...prev, [name]: value }));
+    };
 
     return (
         <>
             <AddTransactionBtn onClick={handleOpen}>
                 <AddIcon />
             </AddTransactionBtn>
-            <Modal open={open} onClose={handleClose}>
-                <Box />
-            </Modal>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Add Transaction</DialogTitle>
+                <DialogActions>
+                    <Toggler>
+                        <ToggleP
+                            className={!transaction.type ? 'green' : 'grey'}
+                        >
+                            Income
+                        </ToggleP>
+                        <ToggleLabel>
+                            <ToggleInput
+                                type="checkbox"
+                                name="type"
+                                onChange={handleInputChange}
+                                checked={transaction.type}
+                            ></ToggleInput>
+                            <ToggleBackground>
+                                <ToggleBtn>
+                                    <AddIcon />
+                                </ToggleBtn>
+                            </ToggleBackground>
+                        </ToggleLabel>
+                        <ToggleP className={transaction.type ? 'pink' : 'grey'}>
+                            Expences
+                        </ToggleP>
+                    </Toggler>
+                </DialogActions>
+                <DialogContent></DialogContent>
+            </Dialog>
         </>
     );
 }
--- src/components/ModalAddTransaction/ModalAddTransaction.styles.js
+++ src/components/ModalAddTransaction/ModalAddTransaction.styles.js
@@ -39,3 +39,80 @@ export const AddIcon = styled.svg`
     width: 20px;
     height: 20px;
 `;
+
+export const Toggler = styled.div`
+    display: flex;
+    gap: 20px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 40px;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ToggleP = styled.p`
+    &.green {
+        color: #24cca7;
+    }
+    &.pink {
+        color: #ff6596;
+    }
+
+    &.grey {
+        color: #e0e0e0;
+    }
+
+    margin: 0;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 1;
+    font-family: Circe, sans-serif;
+`;
+
+export const ToggleLabel = styled.label`
+    position: relative;
+    display: inline-flex;
+    align-items: center;
+    cursor: pointer;
+
+    > input:checked ~ div:first-of-type div {
+        left: 37px;
+        background-color: #ff6596;
+        box-shadow: 0px 6px 15px rgba(255, 101, 150, 0.5);
+    }
+`;
+
+export const ToggleInput = styled.input`
+    clip: rect(0 0 0 0);
+    clip-path: inset(100%);
+    overflow: hidden;
+    position: absolute;
+    white-space: nowrap;
+    width: 1px;
+    height: 1px;
+`;
+
+export const ToggleBackground = styled.div`
+    width: 80px;
+    height: 40px;
+    border-radius: 30px;
+    background-color: #fff;
+    border: 1px solid #e0e0e0;
+    opacity: 1;
+    transition: background-color 200ms ease-in-out;
+`;
+
+export const ToggleBtn = styled.div`
+    position: absolute;
+    top: -2px;
+    left: -1px;
+    width: 44px;
+    height: 44px;
+    background-color: #24cca7;
+    border-radius: 22px;
+    transition: left 250ms ease-in-out;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0px 6px 15px rgba(36, 204, 167, 0.5);
+`;
