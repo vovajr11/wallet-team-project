89d44a1d87b13aad05d968999257efe7cf784df5
Elumion <dovgalyukvlad87@gmail.com>
2022-01-30 23:03:00 +0200

    static component

--- src/components/Statistics/TransictionsList.js
+++ src/components/Statistics/TransictionsList.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { ListOfTransitions, ListItem } from './Statistics.styles'
+import { ListOfTransitions, ListItem, Total } from './Statistics.styles'
 
-export const TransictionsList = () => {
+export const TransictionsList = (props) => {
+
+    const renderItems = (itemsArr) => itemsArr.map(elem => (
+        <ListItem>
+            <div className='squareColor' style={{ backgroundColor: "red" }}></div>
+            <div className='itemInner'>
+                <p>Main expenses</p>
+                <p>8 700.00</p>
+            </div>
+        </ListItem>
+    ))
+
     return (
-        <ListOfTransitions>
-            <ListItem>1</ListItem>
-            <ListItem>2</ListItem>
-            <ListItem>3</ListItem>
-        </ListOfTransitions>
+        <>
+            <ListOfTransitions>
+                {renderItems([1, 2, 3, 4, 5, 6])}
+            </ListOfTransitions>
+            <Total>
+                <p>Expenses:</p>
+                <p className='expenses'>22 549.24</p>
+            </Total>
+            <Total>
+                <p>Incomes:</p>
+                <p className='incomes'>27 350.00</p>
+            </Total>
+        </>
     )
 }
