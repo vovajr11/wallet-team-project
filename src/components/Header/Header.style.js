81c648a144fc3292c387d901e2e289adfee6cba8
Yaroslav Tsiapych <yaroslav121mail@gmail.com>
2022-01-30 21:04:01 +0200

    Header update

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -1,12 +1,27 @@
 import styled from '@emotion/styled';
 
-export const HeaderSection = styled.header`
+export const Header = styled.header`
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     width: 100%;
     padding: 20px 85px;
     background: #fff;
     box-shadow: 0 4px 4px rgba(0, 0, 0, 5%);
+
+    @media screen and (max-width: 768px) {
+        padding: 20px 40px;
+    }
+
+    @media screen and (max-width: 320px) {
+        padding: 15px 20px;
+    }
+`;
+
+export const HeaderInner = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    max-width: 1120px;
 `;
 
 export const HeaderEntrance = styled.ul`
@@ -20,12 +35,9 @@ export const HeaderEntrance = styled.ul`
 export const HeaderEntranceItem = styled.li`
     display: flex;
     align-items: center;
-    font: 18px/1.33 Arial;
+    font: 18px/1.33 'Abel';
     color: #bdbdbd;
     padding-left: 15px;
-
-    &:last-child: {
-    }
 
     &:last-child::before {
         content: '';
@@ -34,10 +46,35 @@ export const HeaderEntranceItem = styled.li`
         height: 30px;
         background: #bdbdbd;
     }
+
+    @media screen and (max-width: 768px) {
+        padding-left: 0px;
+        &:last-child::before {
+            display: none;
+        }
+    }
+`;
+
+export const LogoutBtn = styled.button`
+    background: none;
+    display: flex;
+    align-items: center;
+    border: none;
+
+    &:hover {
+        cursor: pointer;
+    }
 `;
 
 export const LogoutIconW = styled.span`
-    display: flex;
-    align-items: center;
     padding: 0 8px 0 10px;
+`;
+
+export const LogoutBtnText = styled.span`
+    font: 18px/1.33 'Abel';
+    color: #bdbdbd;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
 `;
