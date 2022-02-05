3938d2e5ebec99f4180ac7aeb92c41197e64c028
Elumion <dovgalyukvlad87@gmail.com>
2022-02-05 14:55:01 +0200

    refactor for dinamic update

--- src/components/Statistics/TableStatistics/TableStatistics.js
+++ src/components/Statistics/TableStatistics/TableStatistics.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { default as BasicSelect } from "../Select";
-import { SelectsContainer, TableContainer } from "./TableStatistic.style";
+import React, { useState, useEffect } from 'react';
+import reactDom from 'react-dom';
+import { default as BasicSelect } from "./Select.js";
+import { SelectsContainer, TableContainer } from "./TableStatistic.style.js";
 import { TableTransactions } from './TableTransactions.js';
 import { TransictionsList } from './TransictionsList.js';
 
@@ -8,17 +9,63 @@ import { TransictionsList } from './TransictionsList.js';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 
+const updateUrl = (year, month) => {
+    if (year && month) {
+        return {
+            "year": year,
+            "month": month,
+        }
+    }
+    else if (year) {
+        return {
+            "year": year,
+        }
+    }
+}
 
 const TableStatistics = () => {
+    // https://wallet.goit.ua/api/transactions-summary?month=2&year=2
+
+    let yearNow = (new Date()).getFullYear();
+
+    let [month, setMonth] = useState(null);
+    let [year, setYear] = useState(yearNow);
+    let [fetcher, setFetcher] = useState([]);
+
+
+    const baseURL = `https://wallet.goit.ua/api/transactions-summary`;
+
+    const fetchData = async (params) => {
+        console.log(params);
+        axios.get(baseURL, { params }).then((response) => {
+            setFetcher(response.data);
+        })
+    };
+
+
+    useEffect(() => {
+        let obj = updateUrl(year, month);
+        fetchData(obj);
+        console.log('fetch= ', fetcher)
+    }, [year, month]);
+
+    const setYearOnClick = (value) => {
+        setYear(value);
+    }
+
+    const setMonthOnClick = (value) => {
+        setMonth(value);
+    }
 
     return (
         <TableContainer>
+            {console.log(year)}
             <SelectsContainer>
-                <BasicSelect selectName="Month" />
-                <BasicSelect selectName="Year" />
+                <BasicSelect func={setMonthOnClick} selectName="Month" />
+                <BasicSelect func={setYearOnClick} selectName="Year" />
             </SelectsContainer>
             <TableTransactions />
-            <TransictionsList />
+            <TransictionsList categoriesSummary={fetcher.categoriesSummary} />
         </TableContainer>
     )
 }
--- src/components/Statistics/TableStatistics/TransictionsList.js
+++ src/components/Statistics/TableStatistics/TransictionsList.js
@@ -7,16 +7,16 @@ export const TransictionsList = (props) => {
         <ListItem>
             <div className='squareColor' style={{ backgroundColor: "red" }}></div>
             <div className='itemInner'>
-                <p>Main expenses</p>
-                <p>8 700.00</p>
+                <p>{elem.name}</p>
+                <p>{elem.total}</p>
             </div>
         </ListItem>
-    ))
+    ));
 
     return (
         <>
             <ListOfTransitions>
-                {renderItems([1, 2, 3, 4, 5, 6])}
+                {props.categoriesSummary ? renderItems(props.categoriesSummary) : renderItems([])}
             </ListOfTransitions>
             <Total>
                 <p>Expenses:</p>
