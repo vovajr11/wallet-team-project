1c85574e238ede771e7413c48872a9e8d0357f56
Elumion <dovgalyukvlad87@gmail.com>
2022-02-10 16:50:17 +0200

    refactor transictionslist component

--- src/components/Statistics/TableStatistics/TransictionsList.js
+++ src/components/Statistics/TableStatistics/TransictionsList.js
@@ -1,32 +1,42 @@
-import React from 'react';
-import { ListOfTransitions, ListItem, Total } from './TransictionsList.style'
+import React, { useState } from 'react';
+import { ListOfTransitions, ListItem, Total } from './TransictionsList.style';
+import numberFormater from '../../../logic/numberFormater';
 
 export const TransictionsList = (props) => {
-
-    const renderItems = (itemsArr) => itemsArr.map(elem => (
+    const renderItems = (itemsArr) => itemsArr.map((elem, index) => (
         <ListItem key={elem.name}>
-            <div className='squareColor' style={{ backgroundColor: "red" }}></div>
+            <div className='squareColor' style={{ backgroundColor: props.squareColors[index] }}></div>
             <div className='itemInner' >
                 <p>{elem.name}</p>
-                <p>{elem.total}</p>
+                <p>{numberFormater(elem.total)}</p>
             </div>
         </ListItem>
     ));
 
+    const emptyListMessage = (
+        <ListItem >
+            <div className='itemInner' >
+                <p>Sorry, you have no transactions during this month</p>
+            </div>
+        </ListItem>
+    )
+    console.log("PROPs", props);
+
+    const renderList = () => props.categoriesSummary?.length ? renderItems(props.categoriesSummary) : emptyListMessage;
 
     return (
         <div>
             <ListOfTransitions>
-                {props.categoriesSummary ? renderItems(props.categoriesSummary) : renderItems([])}
+                {renderList()}
             </ListOfTransitions>
             <div>
                 <Total>
                     <p>Expenses:</p>
-                    <p className='expenses'>{props.transactionObj.expenseSummary}</p>
+                    <p className='expenses'>{numberFormater(props.transactionObj.expenseSummary)}</p>
                 </Total>
                 <Total>
                     <p>Incomes:</p>
-                    <p className='incomes'>{props.transactionObj.incomeSummary}</p>
+                    <p className='incomes'>{numberFormater(props.transactionObj.incomeSummary)}</p>
                 </Total>
             </div>
         </div>
