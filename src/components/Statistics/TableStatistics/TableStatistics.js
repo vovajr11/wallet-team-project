32ff60f398ebc1a550ed24bcadf00d21b8c760ca
Elumion <dovgalyukvlad87@gmail.com>
2022-02-11 16:41:46 +0200

    refactor, set months in another file

--- src/components/Statistics/TableStatistics/TableStatistics.js
+++ src/components/Statistics/TableStatistics/TableStatistics.js
@@ -3,36 +3,7 @@ import { default as BasicSelect } from "./Select.js";
 import { SelectsContainer, TableContainer } from "./TableStatistic.style.js";
 import { TableTransactions } from './TableTransactions.js';
 import { TransictionsList } from './TransictionsList.js';
-
-
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
-const monthsObj = {
-    "January": 1,
-    "February": 2,
-    "March": 3,
-    "April": 4,
-    "May": 5,
-    "June": 6,
-    "July": 7,
-    "August": 8,
-    "September": 9,
-    "October": 10,
-    "November": 11,
-    "December": 12,
-}
+import { monthsArr as months } from '../staticObj/montObject.js';
 
 const TableStatistics = (props) => {
 
