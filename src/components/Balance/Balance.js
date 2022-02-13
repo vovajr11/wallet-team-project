795d99e728ce8953a24733a38c9c6c38cf28f5de
Nastia_Chulupyn <nculupin@gmail.com>
2022-02-13 14:24:20 +0200

    Added dynamic Balance

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,13 +1,15 @@
 import { BalanceWrap } from './Balance.styles';
+import { useSelector } from 'react-redux';
+import numberFormater from '../../logic/numberFormater';
 
 const Balance = () => {
-    const balanceValue = '24 000.0';
+    const balanceValue = useSelector(state => state.session.user.balance);
 
     return (
         <BalanceWrap>
             <h3>Your balance</h3>
             <p>
-                <span>₴</span> {balanceValue}
+                <span>₴</span> {numberFormater(balanceValue)}
             </p>
         </BalanceWrap>
     );
