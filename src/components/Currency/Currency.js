ad1eb989a44c57c09a1090e8e082585d74bc90af
Vova Moskalyuk <vova.moskalyuk11jr@gmail.com>
2022-02-16 23:13:45 +0200

    added currency localstorage test

--- src/components/Currency/Currency.js
+++ src/components/Currency/Currency.js
@@ -2,9 +2,21 @@ import { Table } from './Currency.styles';
 import { useState, useEffect } from 'react';
 import Loader from '../Loader/Loader';
 
+// const ONE_HOUR_IN_SECONDS = 1 * 60 * 60 * 1000;
+const ONE_HOUR_IN_SECONDS = 100;
+const CURRENT_TIME = new Date().getTime();
+const deadline =
+    JSON.parse(localStorage.getItem('currency')).currentTime +
+    ONE_HOUR_IN_SECONDS;
+
+const restOfTheTime = deadline - CURRENT_TIME;
+
 const Currency = () => {
-    const [currencyData, setCurrencyData] = useState('');
+    const [currencyData, setCurrencyData] = useState('s');
     const [loading, setLoading] = useState(true);
+
+    const [seconds, setSeconds] = useState(ONE_HOUR_IN_SECONDS);
+    const [timerActive, setTimerActive] = useState(false);
 
     const fetchData = async () => {
         await fetch(
@@ -26,35 +38,67 @@ const Currency = () => {
     };
 
     useEffect(() => {
-        fetchData();
+        if (seconds > 0 && timerActive) {
+            setTimeout(setSeconds, 100, seconds - 1);
+        } else {
+            setTimerActive(false);
+        }
+    }, [seconds, timerActive]);
+
+    useEffect(() => {
+        if (restOfTheTime < 0) {
+            fetchData();
+
+            const obj = {
+                currentTime: CURRENT_TIME,
+                currencyData: currencyData,
+            };
+            localStorage.setItem('currency', JSON.stringify(obj));
+
+            console.log('if');
+        } else {
+            console.log('else');
+            console.log('не робим запит берем старі дані');
+
+            const oldData = localStorage.getItem('currency');
+            console.log(oldData, 'oldData');
+
+            setCurrencyData(oldData);
+            setLoading(false);
+        }
+
         return () => {
             setCurrencyData({});
         };
     }, []);
+
+    console.log(currencyData, 'currencyData');
 
     return <>{loading ? <Loader /> : <TableC data={currencyData} />}</>;
 };
 
 const TableC = ({ data }) => {
     return (
-        <Table>
-            <thead>
-                <tr>
-                    <th>Currency</th>
-                    <th>Purchase</th>
-                    <th>Sale</th>
-                </tr>
-            </thead>
-            <tbody>
-                {[...data].map(currency => (
-                    <tr key={currency.ccy}>
-                        <td>{currency.ccy}</td>
-                        <td>{(+currency.buy).toFixed(2)}</td>
-                        <td>{(+currency.sale).toFixed(2)}</td>
+        <>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Currency</th>
+                        <th>Purchase</th>
+                        <th>Sale</th>
                     </tr>
-                ))}
-            </tbody>
-        </Table>
+                </thead>
+                <tbody>
+                    {[...data].map(currency => (
+                        <tr key={currency.ccy}>
+                            <td>{currency.ccy}</td>
+                            <td>{(+currency.buy).toFixed(2)}</td>
+                            <td>{(+currency.sale).toFixed(2)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </>
     );
 };
 
